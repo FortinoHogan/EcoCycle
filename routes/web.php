b33f3ef19727990a278934e7027d6b2a67fc7347
Steven <steven@binus.edu>
2024-11-28 23:07:41 +0700

    test: midtrans & shop page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\profileController;
 use App\Http\Controllers\ecoforumController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ShopController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,4 +29,8 @@ Route::prefix('/buyer')->group(function(){
 Route::prefix('/seller')->group(function(){
 
 });
+
+Route::get('/payment/snap-token', [PaymentController::class, 'snapToken']);
+Route::get('/payment', [PaymentController::class, 'index']);
+Route::get('/shop', [ShopController::class, 'index']);
 
